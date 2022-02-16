0ebe9731ef87d5be9d41e2ba8d08e2ab47fa1242
Hung <chih-yu.hung@agr.gc.ca>
2022-02-15 20:30:27 -0500

    Original version of simulation

---
+++ Difference of shadow effect on diameter.R
@@ -0,0 +1,62 @@
+#This is to know the shadow ratio in tanks of different diamters
+library(REdaS)
+Latitude<-c(25,35,45)         #latitude
+Htank<-5                      #height of tank
+M.depth<-3                    #fixed manure depth
+ri<-c(5,10,20,30,40)          #Inner radius of tank, m, B32
+T.day.light<-matrix(1:365*5,nrow=365,ncol=5,byrow = T)#save the max light.d
+data<-list()
+#Preparation for heat transfer calculation
+for(k in 1:3){
+L<-Lat[k]
+for(i in 1:5){
+for(j in 1:365){
+Au<-ri[i]^2*pi 
+T.day<-j                                              #DOY 
+T.delta<-300                                          #time step, 300 sec
+T.step<-c(1:288)                                      #vector for time step
+T.second<-seq(300,by=300,length.out=length(T.step))   #vector for seconds
+T.hour<-seq(300/3600,24,length.out=length(T.step))    #vector for hours
+H<-15*(T.hour-12)                                     #a coefficient related to angle of sunlight
+
+#Radiative heat transfer
+declination.s<-23.45*sin((2*pi*(284+T.day)/365))               # seasonal declination(degree)
+sin.alpha<-pmax((cos(deg2rad(L))*cos(deg2rad(declination.s))
+                 *cos(deg2rad(H))+sin(deg2rad(L))
+                 *sin(deg2rad(declination.s))),0)              # sunlight degree
+
+#This's a part to calculate shadow area due to the tank wall, it's not in Rennie, 2017
+wall.h<-Htank-M.depth                                 # the wall height above manure surface, m
+cot.alpha<-(1-sin.alpha^2)^(1/2)/sin.alpha
+cos.theta<-(wall.h*cot.alpha/2)/ri[i]                 # the angle in the circle-circle intersection, a numeric
+deg.theta<-acos(cos.theta)
+Intersection.h<-ri[i]*(1-cos.theta^2)^(1/2)           # the height of triangle in the circle-circle intersection, m
+shadow<-pi*ri[i]^2-(4*pi*ri[i]^2*deg.theta/(2*pi)
+                 -4*(wall.h*cot.alpha)/2*Intersection.h/2)  # shadow area, m2
+light.d<-1-(shadow/Au)                                # the percentage that sunlight on the surface, between 0-1
+light.d[is.nan(light.d)]<-0
+###End for shadow calculation
+Sb<-ifelse(sin.alpha>0, Eb*(tau^m)*sin.alpha,0)       # solar bean radiation (W/m2),F104-KG104
+
+cat(c(i,j,max(light.d)))
+T.day.light[j,i]<-sum(light.d[light.d!=0])
+data[[k]]<-T.day.light
+}
+}
+}
+
+par(mfrow=c(1,3))
+for (k in 1:3){
+cols<-c("#a6bddb","#74a9cf","#3690c0",
+        "#0570b0","#034e7b")
+plot(data[[k]][,5],type="l",col=cols[5],
+     xlab="Day of year",ylab="Percentage of light to the surface")
+for (i in 1:4) {
+lines(data[[k]][,i],col=cols[i])
+}
+legend(50,0.5,c("5m","10m","20m","30m","40m"),col=cols,lty=1,
+       title="tank radius")
+mtext(paste("Latitude=",Lat[k],sep=""),side=3,line =1)
+}
+
+A<-111
